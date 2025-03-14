2da37d2d176e463a020c04a9453a8cf7200d2fae
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-03-14 14:25:35 +0100

    Remove env from actions

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ module "github-repo-2" {
 
 }
 
+module "github-repo" {
+  source = "./modules/repo"
+
+  name        = "created-terraform-repo"
+  description = "This is my repo created automatically by Terraform"
+  visibility  = "private"
+
+}
+
+
