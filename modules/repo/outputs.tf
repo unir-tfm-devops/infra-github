e7cfc505b2c36d4df0f670d1abff136f7f7d8dbf
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-03-14 13:20:20 +0100

    Remove clone url

--- modules/repo/outputs.tf
+++ modules/repo/outputs.tf
@@ -2,8 +2,3 @@ output "repo_url" {
   description = "The URL of the created GitHub repository"
   value       = github_repository.this.html_url
 }
-
-output "repo_clone_url" {
-  description = "The Git clone URL of the repository"
-  value       = github_repository.this.clone_url
-}
