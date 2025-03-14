9080f8915f33aa0aa8b77029a97f2a2a74f712a5
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-03-14 14:31:24 +0100

    Remove other repositories

--- main.tf
+++ main.tf
@@ -1,25 +1,7 @@
-module "github-repo-2" {
-  source = "./modules/repo"
-
-  name        = "created-terraform-repo-2"
-  description = "This is my repo created automatically by Terraform"
-  visibility  = "private"
-
-}
-
 module "github-repo" {
   source = "./modules/repo"
 
   name        = "created-terraform-repo"
-  description = "This is my repo created automatically by Terraform"
-  visibility  = "private"
-
-}
-
-module "github-repo-3" {
-  source = "./modules/repo"
-
-  name        = "created-terraform-repo-3"
   description = "This is my repo created automatically by Terraform"
   visibility  = "private"
 
