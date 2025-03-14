81f552670c7850e7c38ec3b3982278631346d460
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-03-14 14:06:40 +0100

    New repository

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ module "github_repo" {
   visibility  = "private"
 
 }
+
+module "github-repo-2" {
+  source = "./modules/repo"
+
+  name        = "created-terraform-repo-2"
+  description = "This is my repo created automatically by Terraform"
+  visibility  = "private"
+
+}
+
