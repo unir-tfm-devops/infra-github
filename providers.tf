0f648e29b819310220588bc71b0a04e49c9cfd66
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-03-14 13:17:08 +0100

    Terraform basic configuration

---
+++ providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud {
+    organization = "unir-tfm"
+
+    workspaces {
+      name = "unir-tfm-terraform-github"
+    }
+  }
+
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+}
