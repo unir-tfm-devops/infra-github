847493d2b32dbb7d114f5cb026e6e641e245f12c
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-05-08 18:42:20 +0200

    TFM-1 | Removed old repositories (#39)

--- repos.tf
+++ repos.tf
@@ -1,15 +1,1 @@
-module "products-search" {
-  source = "./modules/repo"
-
-  name        = "products-search"
-  description = "This is my repo created automatically by Terraform"
-  visibility  = "private"
-
-}
-
-module "spring-boot-template" {
-  source      = "./modules/repo"
-  name        = "spring-boot-template"
-  description = "Repository for the spring boot template"
-  visibility  = "public"
-}
+# File with repositories for the organization
