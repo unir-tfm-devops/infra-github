686bed96cf07a950c284d0a822de83c8f3f42c29
Alvaro Rodriguez <alvamige@gmail.com>
2025-05-08 19:58:57 +0200

    TFM-1 | Remove all repositories

--- repos.tf
+++ repos.tf
@@ -1,14 +1,1 @@
 # File with repositories for the organization
-module "products-search" {
-  source      = "./modules/repo"
-  name        = "products-search"
-  description = "Products Search repository"
-  visibility  = "public"
-}
-
-module "products-search-1" {
-  source      = "./modules/repo"
-  name        = "products-search-new"
-  description = "Products Search repository new"
-  visibility  = "public"
-}
