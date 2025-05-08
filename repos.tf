8d5f37b63d59911151df9d865ca851b2c1ad9505
Alvaro Rodriguez <alvamige@gmail.com>
2025-05-08 19:49:51 +0200

    TFM-1 | Add new repo

--- repos.tf
+++ repos.tf
@@ -5,3 +5,10 @@ module "products-search" {
   description = "Products Search repository"
   visibility  = "public"
 }
+
+module "products-search-1" {
+  source      = "./modules/repo"
+  name        = "products-search-new"
+  description = "Products Search repository new"
+  visibility  = "public"
+}
