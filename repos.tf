b00b55224c42761432c0384a23631be3e87bf314
Alvaro Rodriguez <alvamige@gmail.com>
2025-05-08 19:39:22 +0200

    TFM-1 | Modify workflow to update repos.tf

--- repos.tf
+++ repos.tf
@@ -1,1 +1,7 @@
 # File with repositories for the organization
+module "products-search" {
+  source      = "./modules/repo"
+  name        = "products-search"
+  description = "Products Search repository"
+  visibility  = "public"
+}
