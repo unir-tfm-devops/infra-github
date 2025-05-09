8124cda9ca24fd241e0fa45d28a950f5a882a925
Alvaro Rodriguez <alvamige@gmail.com>
2025-05-09 13:30:49 +0200

    TFM-1 | Remove existing repository

--- repos.tf
+++ repos.tf
@@ -1,8 +1,1 @@
 # File with repositories for the organization
-
-module "terraform-aks" {
-  source      = "./modules/repo"
-  name        = "terraform-aks"
-  description = "Repository for AKS configuration"
-  visibility  = "public"
-}
