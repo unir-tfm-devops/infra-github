2ee32d04b11b8efcbe0b75d1e915313621e2350d
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-06-24 17:53:23 +0200

    TFM-7 | Fix error when checking out repository

--- api-repos.tf
+++ api-repos.tf
@@ -1,9 +1,9 @@
 # File with REST API repositories
 
-module "products-search-api-2" {
+module "products-search-api-3" {
   source      = "./modules/repo"
-  name        = "products-search-api-2"
-  description = "products-search-api-2"
+  name        = "products-search-api-3"
+  description = "products-search-api-3"
   visibility  = "public"
   template_owner = "unir-tfm-devops"
   template_repository = "spring-boot-template"
