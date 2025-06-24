e1dcaf17a0da3312202980cf1a28fca3b53b283f
Alvaro Rodriguez Gonzalez <alvamige@gmail.com>
2025-06-24 18:03:04 +0200

    TFM-7 | Remove existing repository

--- api-repos.tf
+++ api-repos.tf
@@ -1,23 +1,3 @@
 # File with REST API repositories
 
-module "products-search-api-4" {
-  source      = "./modules/repo"
-  name        = "products-search-api-4"
-  description = "products-search-api-4"
-  visibility  = "public"
-  template_owner = "unir-tfm-devops"
-  template_repository = "spring-boot-template"
-
-  environments = {
-    test = {
-      name = "test"
-      reviewers = []
-    }
-    production = {
-      name = "production"
-      reviewers = [module.admins.id]
-    }
-  }
-}
-
 
